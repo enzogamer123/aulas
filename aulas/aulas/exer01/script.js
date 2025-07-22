285bf49c278cd4c5972f0f7ea03bd1de6f3187a6
enzogamer123 <badfuture19@gmail.com>
2025-07-22 12:26:12 -0300

    Add files via upload

---
+++ aulas/aulas/exer01/script.js
@@ -0,0 +1,7 @@
+function soma(){
+    var numero1 = document.getElementById("soma").valueAsNumber
+    var numero2 = document.getElementById("soma").valueAsNumber
+    console.log(numero1,numero2)
+    document.getElementById('resposta').textContent = numero1 + numero2
+
+}
